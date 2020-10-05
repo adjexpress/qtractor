1ebde09347cea9321ae1526103d0762e947bdf48
mohsen <thelight.mn@gmail.com>
2020-10-06 02:37:28 +0330

    Update build script

--- build.sh
+++ build.sh
@@ -5,13 +5,26 @@ if [[ $1 == 'debug' || $1 == 'release' ]]; then
     cd build/$1
     qmake ../../traqtor.pro -spec linux-g++ CONFIG+=$1 CONFIG+=qml_debug
     make -j4
+    if [[ $? -eq 0 ]]; then
+        echo -e "\n---> Output built in `pwd`"
+    fi
 elif [[ $1 == 'clean' ]]; then
     if [[ -e build/debug ]]; then
         cd build/debug
         make clean -j4
+        if [[ $? -eq 0 ]]; then
+            echo -e "\n---> Clean done."
+        fi
     else
-        echo "There's no debug dir."
+        echo "---> There's no debug dir."
+    fi
+elif [[ $1 == 'run' ]]; then
+    if [[ -e build/debug/traqtor/traqtor ]]; then
+        './build/debug/traqtor/traqtor'
+    else
+        echo "---> There isn't file (./build/debug/traqtor/traqtor) to run"
     fi
 else
-    echo "Inappropriate arguments!"
+    echo "---> Inappropriate commands!"
+    echo "---> Available commands: debug | release | clean | run"
 fi
